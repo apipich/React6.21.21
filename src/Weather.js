c4644fedd23e786188781e7798936a9ab2a59f4c
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-06-24 11:23:44 -0400

    Created event listeners on form to get value of what is searched, and then to make api call of that city

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./styles.css";
-
 import Condition from "./Condition";
 
 export default function Weather(props) {
 const [load, setLoad] = useState(false);
 const [allWeather, setAllWeather] = useState({});
+const [city, setCity] = useState(props.primaryCity);
 
 function showResponse(response) {
 setAllWeather({
@@ -22,12 +22,27 @@ setAllWeather({
 setLoad(true);
 }
 
+function search(){
+const apiKey = "44586293a46c054713f47fc69d4f2a52";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`
+axios.get(apiUrl).then(showResponse);
+}
+
+function handleSubmit(event) {
+event.preventDefault();
+search(city);
+}
+
+function changeCity(event) {
+setCity(event.target.value);
+}
+
 if (load === true) {
 return (
   <div className="totalWeather">
 <Condition data={allWeather}/>
       <div className="form">
-        <form id="weather-form">
+        <form id="weather-form" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-3">
               <input
@@ -35,6 +50,7 @@ return (
                 id="city-input"
                 placeholder="Enter a city"
                 autoFocus="on"
+onChange={changeCity}
               />
             </div>
             <div className="col-3">
@@ -50,10 +66,8 @@ return (
 
 );
 } else {
-const apiKey = "44586293a46c054713f47fc69d4f2a52";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.primaryCity}&appid=${apiKey}&units=imperial`
-axios.get(apiUrl).then(showResponse);
 
+search(city);
 return "Loading...";
 }
 }

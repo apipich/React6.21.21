ab638a09bd7489d333764783ec9a2912e5cbbdb2
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-06-28 15:53:16 -0400

    created new function, Day() to get exact first day of forecast

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -2,9 +2,18 @@ import React from "react";
 
 export default function DailyWeather(props) {
 
+  function day() {
+let date = new Date(props.data.dt * 1000);
+let day = date.getDay();
+
+let days = ["SUN", "MON", "TUES", "WED", "THUR", "FRI", "SAT"];
+
+return days[day];
+  }
+
   return (
     <div>
-  <div className="weather-forecast-day">{props.data.dt}</div>
+  <div className="weather-forecast-day">{day()}</div>
   <div className="weather-forecast-condition" id="forecastNumber">{props.data.weather[0].description}</div>
   <div className="icon"><img src={`https://openweathermap.org/img/wn/${props.data.weather[0].icon}@2x.png`} alt="icon" width="55"/>
   </div>

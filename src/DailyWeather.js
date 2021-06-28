8e317ddad1cd755dea24404ea5a9a4b3533dce49
Amanda Pipich <79532564+apipich@users.noreply.github.com>
2021-06-28 15:32:14 -0400

    playing around with icon ID

--- src/DailyWeather.js
+++ src/DailyWeather.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function DailyWeather(props) {
+let iconId = <img src="https://openweathermap.org/img/wn/{props.data.weather[0].icon}@2x.png" alt="icon"/>
+
   return (
     <div>
   <div className="weather-forecast-day">{props.data.dt}</div>
   <div className="weather-forecast-condition" id="forecastNumber">{props.data.weather[0].description}</div>
-  <div className="icon">icon</div>
+  <div className="icon">{iconId}</div>
   <div className="weather-forecast-temperature">
     <div className="weather-forecast-temp">{Math.round(props.data.temp.day)}°</div>
 </div>
